ce8c497aee76ddac1a55946d300b5ad556ede843
Avishkar Tambe <tambeavishkar1@gmail.com>
2026-07-03 02:13:43 +0530

    Fix Vercel build errors

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from "./components/About/About";
 import Skills from "./components/Skills/Skills";
 import Projects from "./components/Projects/Projects";
 import Experience from "./components/Experience/Experience";
-// import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Futer";
 import Certifications from "./components/Certifications/Certifications";
