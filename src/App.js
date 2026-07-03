c9f29317eff438b15e615b41eef3ffc62c58b29d
Avishkar Tambe <tambeavishkar1@gmail.com>
2026-07-03 15:00:30 +0530

    Test App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h1>✅ Portfolio is Working!</h1>
+      <p>If you can see this, the deployment is fine.</p>
+    </div>
+  );
+}
+
+export default App;
