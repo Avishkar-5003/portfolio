e2bfeb806dba56fc901eb60175c961e1e5eb3e58
Avishkar Tambe <tambeavishkar1@gmail.com>
2026-07-03 15:08:02 +0530

    Test App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 
+// Components
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
+import Experience from "./components/Experience/Experience";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Futer";
+import Certifications from "./components/Certifications/Certifications";
+import "./App.css";
+
 function App() {
   return (
-    <div style={{ padding: "50px", textAlign: "center" }}>
-      <h1>✅ Portfolio is Working!</h1>
-      <p>If you can see this, the deployment is fine.</p>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Certifications />
+      <Contact />
+      <Footer />
+
+      
+    </>
   );
 }
 
