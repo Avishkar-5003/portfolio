bc2782d4ba898d77f2b8b859ef60278faec006c5
Avishkar Tambe <tambeavishkar1@gmail.com>
2026-07-03 15:25:43 +0530

    Add profile image

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -41,7 +41,7 @@ function Hero() {
       </div>
 
       <div className="hero-image">
-        <img src="/images/profile.png" alt="Avishkar Tambe" />
+        <img src="/images/profile.jpg" alt="Avishkar Tambe" />
       </div>
     </section>
   );
